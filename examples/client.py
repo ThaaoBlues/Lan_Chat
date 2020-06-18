1da0ef81e733be9436de713462acf99493554ae3
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2020-06-18 22:08:04 +0200

    Create client.py

---
+++ examples/client.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+
+    #init chat client
+    client1 = chat_client(port = 8835, username = "ThaaoBlues", server_ip = "127.0.0.1")
+
+    #connect to the server
+    client1.connect_to_chat()
+
+    #infinite loop to send messages 
+    #with a refresh delay to not call all the print functions at the same time
+    while True:
+        time.sleep(0.5)     
+        client1.send_msg(message = input("You : "))
