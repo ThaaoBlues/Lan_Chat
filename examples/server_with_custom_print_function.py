a2b18592161502d64bcfd44b39673cce772c49fa
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2020-06-19 19:11:28 +0200

    Create server_with_custom_print_function.py

---
+++ examples/server_with_custom_print_function.py
@@ -0,0 +1,13 @@
+def print_message_function(message):
+    print(message)
+
+
+
+
+if __name__ == "__main__":
+    #create an object server, specify the port to use and the maximum number of hosts.
+    server1 = chat_server(port = 8835, max_hosts = 10, get_msg = "print_message_function")
+
+    print("Starting server...")
+    #litteraly starting the server like I said above
+    server1.start_server()
