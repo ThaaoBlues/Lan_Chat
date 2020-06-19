395435db99e1e217b172888bd896f428485535d5
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2020-06-19 19:08:43 +0200

    Update server.py

--- examples/server.py
+++ examples/server.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
     #create an object server, specify the port to use and the maximum number of hosts.
-    server1 = chat_server(port = 8835, max_hosts = 10)
+    server1 = chat_server(port = 8835, max_hosts = 10, get_msg = "")
 
     print("Starting server...")
     #litteraly starting the server like I said above
