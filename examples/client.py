2bd4f4d0363d5ff090b796538ab9ee31773c0324
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2020-06-25 13:52:16 +0200

    Update client.py

--- examples/client.py
+++ examples/client.py
@@ -3,7 +3,7 @@ from lan_chat.chat_client import chat_client
 if __name__ == "__main__":
 
     #init chat client
-    client1 = chat_client(port = 8835, username = "ThaaoBlues", server_ip = "127.0.0.1", get_msg = "")
+    client1 = chat_client(port = 8835, username = "ThaaoBlues",server_ip = input("server IP address : "), get_msg = "")
 
     #connect to the server
     client1.connect_to_chat()
